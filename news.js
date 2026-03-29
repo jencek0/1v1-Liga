f97522bf88a5ee1c3bdba3951ceeb1daa5355b5b
jencek0 <navrariljan002@gmail.com>
2026-03-29 13:58:55 +0200

    Add files via upload

---
+++ news.js
@@ -0,0 +1,37 @@
+const newsData = [
+  
+  {
+    date: "21. 3. 2026",
+    title: "Začátek jarní části ligy",
+    text: "Bylo oznámeno datum prvního zápasového dne jarní části 1v1 ligy, které bylo změněno z 28. března na 11.-12. dubna. První zápas odehraje ŠK Slovan Bratislava s 1.FC Slovácko. Místo konání celého zápasového dne bude upřesněno."
+  },
+  {
+    date: "24. 11. 2025",
+    title: "Vyhlášení výherců ocenění",
+    text: "Byly vyhlášeny ankety sezóny 2025. Hráčem sezóny se stal Jencek. Nejhezčí gól vstřelil Ondřej."
+  },
+  {
+    date: "18. 10. 2025",
+    title: "První kolo odehráno",
+    text: "Nová tvář ligy AC Sparta Praha porazila domácí 1.FC Slovácko za výsledku 1:6, stejné skóre bylo i na konci 2. zápasu ve kterém tým <br>ŠK Slovan Bratislava podlehl Barceloně. Ta ve svém 2. utkání výherně navázala a porazila tým FK Hodonín 5:1."
+  },
+  {
+    date: "17. 10. 2025",
+    title: "Nový člen",
+    text: "Byl oficiálně ohlášen nový člen ligy, a to tým FK Hodonín. Ten se tak společně s celkem AC Sparta Praha stane nováčkem soutěže a nahradí tým Šaška."
+  },
+  {
+    date: "10. 10. 2025",
+    title: "Legenda ukončila kariéru?",
+    text: "Hráčská legenda pod přezdívkou David Neres, Šašek, ze zdravotních důvodů ukončil svou kariéru. Jeho tým tak odstupuje nejen z 1v1 Ligy, ale také všech soutěží zřizovaných Svazem Malého Fotbalu. Kvůli této ztrátě hráče který ve své první a jediné sezóně nastřílel 23 gólů za 8 zápasů bude Svaz hledat náhradu do další sezóny."
+  },
+  {
+    date: "1. 9. 2025",
+    title: "Rozlosování sezóny potvrzeno",
+    text: "Bylo potvrzeno rozlosování základní části nové sezóny 1v1 ligy. <br> První duel mezi Slováckem a Spartou se odehraje již 18. října."
+  },
+  {
+    date: "27. 8. 2025",
+    title: "Liga se rozroste",
+    text: "Podle nejnovějších informací by se 1v1 Liga měla rozrůst o 1 tým. Tím by měl být AC Sparta Praha který se již prezentoval výhrou 1. ročníku SuperCupu poté co po dobu celého turnaje naprosto dominoval. Je tak jasné že již v první sezóně bude mít ambice útočit na titul."
+  }, ];
