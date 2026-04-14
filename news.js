9b9150a295db1d4aca948726abb4581f9f65bb7e
jencek0 <navrariljan002@gmail.com>
2026-04-14 20:43:05 +0200

    Update news.js

--- news.js
+++ news.js
@@ -1,35 +1,60 @@
 const newsData = [
   {
+    season: "2025/26",
+    date: "12. 04. 2026",
+    title: "Matchday 2 je za námi",
+    text: "Byl odehrán 2. zápasový den, který přinesl 3 gólově bohaté zápasy. Jednalo se o dohrávku 2. kola mezi celky ŠK Slovan Bratislava a 1.FC Slovácko ve kterém Slovan zvítězil 5:3, a poté o předehrávky 4. a 6. kola ve kterých si FC Barcelona nejprve zajistila 3 body proti Slovácku když dala vítězný gól na 4:3 v nastaveném čase a poté si poradila se Slovanem výsledkem 8:5."
+  },
+  {
+    season: "2025/26",
+    date: "12. 04. 2026",
+    title: "Změna dnešního matchdaye",
+    text: "Čas výkopu byl po Ondřejově požadavku přesunut na 13:00. Zároveň jsme dostali informaci že se dnes nedostaví Lily, a nejspíše ani Pája. Budou tedy předehrány zápasy mezi celky FC Barcelona, ŠK Slovan Bratislava a 1.FC Slovácko. Počet hraných zápasů bude rozhodnut na místě."
+  },
+  {
+    season: "2025/26",
+    date: "10. 4. 2026",
+    title: "Matchday 2",
+    text: "Blíží se nám 2. Matchday 1v1 Ligy, který zahájí celky ŠK Slovan Bratislava a 1.FC Slovácko. Datum bylo upřesněno na neděli 12. dubna. Hrát se bude na hřišti Za Barákem. Zápas začíná ve 14 hodin a na místě by měli být všechny celky."
+  },
+  {
+    season: "2025/26",
     date: "21. 3. 2026",
     title: "Začátek jarní části ligy",
     text: "Bylo oznámeno datum prvního zápasového dne jarní části 1v1 ligy, které bylo změněno z 28. března na 11.-12. dubna. První zápas odehraje ŠK Slovan Bratislava s 1.FC Slovácko. Místo konání celého zápasového dne bude upřesněno."
   },
   {
+    season: "2025/26",
     date: "24. 11. 2025",
     title: "Vyhlášení výherců ocenění",
     text: "Byly vyhlášeny ankety sezóny 2025. Hráčem sezóny se stal Jencek. Nejhezčí gól vstřelil Ondřej."
   },
   {
+    season: "2025/26",
     date: "18. 10. 2025",
     title: "První kolo odehráno",
     text: "Nová tvář ligy AC Sparta Praha porazila domácí 1.FC Slovácko za výsledku 1:6, stejné skóre bylo i na konci 2. zápasu ve kterém tým <br>ŠK Slovan Bratislava podlehl Barceloně. Ta ve svém 2. utkání výherně navázala a porazila tým FK Hodonín 5:1."
   },
   {
+    season: "2025/26",
     date: "17. 10. 2025",
     title: "Nový člen",
     text: "Byl oficiálně ohlášen nový člen ligy, a to tým FK Hodonín. Ten se tak společně s celkem AC Sparta Praha stane nováčkem soutěže a nahradí tým Šaška."
   },
   {
+    season: "2025/26",
     date: "10. 10. 2025",
     title: "Legenda ukončila kariéru?",
     text: "Hráčská legenda pod přezdívkou David Neres, Šašek, ze zdravotních důvodů ukončil svou kariéru. Jeho tým tak odstupuje nejen z 1v1 Ligy, ale také všech soutěží zřizovaných Svazem Malého Fotbalu. Kvůli této ztrátě hráče který ve své první a jediné sezóně nastřílel 23 gólů za 8 zápasů bude Svaz hledat náhradu do další sezóny."
   },
   {
+    season: "2025/26",
     date: "1. 9. 2025",
     title: "Rozlosování sezóny potvrzeno",
     text: "Bylo potvrzeno rozlosování základní části nové sezóny 1v1 ligy. <br> První duel mezi Slováckem a Spartou se odehraje již 18. října."
   },
   {
+    season: "2025/26",
     date: "27. 8. 2025",
     title: "Liga se rozroste",
     text: "Podle nejnovějších informací by se 1v1 Liga měla rozrůst o 1 tým. Tím by měl být AC Sparta Praha který se již prezentoval výhrou 1. ročníku SuperCupu poté co po dobu celého turnaje naprosto dominoval. Je tak jasné že již v první sezóně bude mít ambice útočit na titul."
